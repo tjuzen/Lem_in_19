61891daf5f2439e00508af9a6375fceb824e60b8
tjuzen <444Creve-biscotte>
2019-11-13 21:26:19 +0100

    check moi caaaa

--- src/algo.c
+++ src/algo.c
@@ -6,7 +6,7 @@
 /*   By: tjuzen <tjuzen@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 20:51:04 by tjuzen            #+#    #+#             */
-/*   Updated: 2019/11/13 21:22:01 by tjuzen           ###   ########.fr       */
+/*   Updated: 2019/11/13 21:25:51 by tjuzen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,87 +26,87 @@ void print_debug(t_data_map *map, t_lemin *arg)
 	t_node *j = lookup(map, hashCode("#d"), "#d");
 
 	a->weight = INFINITE;
-b->weight = INFINITE;
-c->weight = INFINITE;
-d->weight = INFINITE;
-e->weight = INFINITE;
-f->weight = INFINITE;
-g->weight = INFINITE;
-h->weight = INFINITE;
-i->weight = INFINITE;
-j->weight = INFINITE;
+	b->weight = INFINITE;
+	c->weight = INFINITE;
+	d->weight = INFINITE;
+	e->weight = INFINITE;
+	f->weight = INFINITE;
+	g->weight = INFINITE;
+	h->weight = INFINITE;
+	i->weight = INFINITE;
+	j->weight = INFINITE;
+	arg->start->weight = 0;
 
-arg->start->weight = 0;
-	printf("\n----------------\nLiaisons partant de %s-%c ayant %s en path\n----------------\n", a->room, a->type, a->path->room);
-	while (a->to)
-	{
-		if (a->to->isactive)
-		printf("%s-%c ", a->to->roomb->room, a->to->roomb->type);
-		a->to = a->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", b->room, b->type);
-	while (b->to)
-	{
-		if (b->to->isactive)
-		printf("%s-%c ", b->to->roomb->room, b->to->roomb->type);
-		b->to = b->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", c->room, c->type);
-	while (c->to)
-	{
-		if (c->to->isactive)
-		printf("%s-%c ", c->to->roomb->room, c->to->roomb->type);
-		c->to = c->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", d->room, d->type);
-	while (d->to)
-	{
-		if (d->to->isactive)
-		printf("%s-%c ", d->to->roomb->room, d->to->roomb->type);
-		d->to = d->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", e->room, e->type);
-	while (e->to)
-	{
-		if (e->to->isactive)
-		printf("%s-%c ", e->to->roomb->room, e->to->roomb->type);
-		e->to = e->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", f->room, f->type);
-	while (f->to)
-	{
-		if (f->to->isactive)
-		printf("%s-%c ", f->to->roomb->room, f->to->roomb->type);
-		f->to = f->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", g->room, g->type);
-	while (g->to)
-	{
-		if (g->to->isactive)
-		printf("%s-%c ", g->to->roomb->room, g->to->roomb->type);
-		g->to = g->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", h->room, h->type);
-	while (h->to)
-	{
-		if (h->to->isactive)
-		printf("%s-%c ", h->to->roomb->room, h->to->roomb->type);
-		h->to = h->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", i->room, i->type);
-	while (i->to)
-	{
-		if (i->to->isactive)
-		printf("%s-%c ", i->to->roomb->room, i->to->roomb->type);
-		i->to = i->to->nexto;
-	}
-	printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", j->room, j->type);
-	while (j->to)
-	{
-		if (j->to->isactive)
-		printf("%s-%c ", j->to->roomb->room, j->to->roomb->type);
-		j->to = j->to->nexto;
-	}
+	// printf("\n----------------\nLiaisons partant de %s-%c ayant %s en path\n----------------\n", a->room, a->type, a->path->room);
+	// while (a->to)
+	// {
+	// 	if (a->to->isactive)
+	// 	printf("%s-%c ", a->to->roomb->room, a->to->roomb->type);
+	// 	a->to = a->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", b->room, b->type);
+	// while (b->to)
+	// {
+	// 	if (b->to->isactive)
+	// 	printf("%s-%c ", b->to->roomb->room, b->to->roomb->type);
+	// 	b->to = b->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", c->room, c->type);
+	// while (c->to)
+	// {
+	// 	if (c->to->isactive)
+	// 	printf("%s-%c ", c->to->roomb->room, c->to->roomb->type);
+	// 	c->to = c->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", d->room, d->type);
+	// while (d->to)
+	// {
+	// 	if (d->to->isactive)
+	// 	printf("%s-%c ", d->to->roomb->room, d->to->roomb->type);
+	// 	d->to = d->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", e->room, e->type);
+	// while (e->to)
+	// {
+	// 	if (e->to->isactive)
+	// 	printf("%s-%c ", e->to->roomb->room, e->to->roomb->type);
+	// 	e->to = e->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", f->room, f->type);
+	// while (f->to)
+	// {
+	// 	if (f->to->isactive)
+	// 	printf("%s-%c ", f->to->roomb->room, f->to->roomb->type);
+	// 	f->to = f->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", g->room, g->type);
+	// while (g->to)
+	// {
+	// 	if (g->to->isactive)
+	// 	printf("%s-%c ", g->to->roomb->room, g->to->roomb->type);
+	// 	g->to = g->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", h->room, h->type);
+	// while (h->to)
+	// {
+	// 	if (h->to->isactive)
+	// 	printf("%s-%c ", h->to->roomb->room, h->to->roomb->type);
+	// 	h->to = h->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", i->room, i->type);
+	// while (i->to)
+	// {
+	// 	if (i->to->isactive)
+	// 	printf("%s-%c ", i->to->roomb->room, i->to->roomb->type);
+	// 	i->to = i->to->nexto;
+	// }
+	// printf("\n----------------\nLiaisons partant de %s-%c\n----------------\n", j->room, j->type);
+	// while (j->to)
+	// {
+	// 	if (j->to->isactive)
+	// 	printf("%s-%c ", j->to->roomb->room, j->to->roomb->type);
+	// 	j->to = j->to->nexto;
+	// }
 	printf("\n\nTotal nodes %lu\n", arg->totalrooms);
 }
 
@@ -177,10 +177,10 @@ t_linkstab *try;
 		if (in->to->isactive)
 		{
 			try = lookuplink(map, in, in->to->roomb);
-			printf("Mon link %c%s %c%s\n", try->rooma->type, try->rooma->room, try->roomb->type, try->roomb->room);
+			// printf("Mon link %c%s %c%s\n", try->rooma->type, try->rooma->room, try->roomb->type, try->roomb->room);
 			if (try->roomb->path == try->rooma)
 			{
-				printf("Je passe \n");
+				// printf("Je passe \n");
 				// printf("Je remplace %s par %s\n", try->rooma->room, try->roomb->room);
 				// try->rooma = out;
 			}
@@ -265,13 +265,13 @@ int bellman_peugeot(t_data_map *map, t_lemin *arg)
 		{
 			if (link->isactive == 1)
 			{
-				printf("Je teste %s (%i) %s (%i)\n", link->rooma->room, link->rooma->weight, link->roomb->room, link->roomb->weight);
+				// printf("Je teste %s (%i) %s (%i)\n", link->rooma->room, link->rooma->weight, link->roomb->room, link->roomb->weight);
 				if (link->rooma->weight != INFINITE && link->rooma->weight + link->weight < link->roomb->weight && link->roomb != arg->start && link->isactive == 1)
 				{
 					link->roomb->weight = link->weight + link->rooma->weight;
 					link->roomb->parent = link->rooma;
 					link->rooma->child = link->roomb;
-					printf("%s est le parent de %s\n", link->roomb->parent->room, link->roomb->room);
+					// printf("%s est le parent de %s\n", link->roomb->parent->room, link->roomb->room);
 				}
 			}
 			link = link->next;
@@ -283,7 +283,7 @@ int bellman_peugeot(t_data_map *map, t_lemin *arg)
 		if (link->rooma->weight != INT_MAX - 10 && link->rooma->weight + link->weight < link->roomb->weight)
 		{
 			printf("NEGATIF WEIGHT CYCLE AAAAAH\n");
-			printf("oo %s %s \n", link->rooma->room, link->roomb->room);
+			// printf("oo %s %s \n", link->rooma->room, link->roomb->room);
 		}
 		link = link->next;
 	}
@@ -300,7 +300,6 @@ int find_path(t_data_map *map, t_lemin *arg)
 		return (-1);
 	duppp(map, arg);
 	print_all_links(map, arg, map->links);
-	printf("Si segfault commenter la fonction print_debug()\n");
 	print_debug(map, arg);
 	// printf("\n");
 	bellman_peugeot(map, arg);
